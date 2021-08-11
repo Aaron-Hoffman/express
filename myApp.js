5b15e791de54b8f5a6f85c691e7ad9b99cb61844
AaronHoffman <aahoffm@gmail.com>
2021-08-11 12:29:15 -0400

    serve string

--- myApp.js
+++ myApp.js
@@ -1,7 +1,13 @@
 var express = require('express');
+//  Creates express app object
 var app = express();
 
 console.log('Hello World');
+
+//  Serves string to get requests to the root directory
+app.get('/', function(req, res) {
+    res.send('Hello Express');
+})
 
 
 
