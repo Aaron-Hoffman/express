033e5bd0ba8d52e01a7cff64ffe6d8fc67447d69
AaronHoffman <aahoffm@gmail.com>
2021-08-11 13:12:33 -0400

    env working

--- myApp.js
+++ myApp.js
@@ -1,67 +1,35 @@
-var express = require('express');
+var express = require("express");
 //  Creates express app object
 var app = express();
 
-console.log('Hello World');
+console.log("Hello World");
 
 // absolute path for index.html
-const indexPath = __dirname + '/views/index.html' 
+const indexPath = __dirname + "/views/index.html";
 //  absolute path for public folder
-const publicPath = __dirname + '/public'
+const publicPath = __dirname + "/public";
 
 // allows access to public folder
 app.use("/public", express.static(publicPath));
 
 //  Serves index.html to get requests to the root directory
-app.get('/', function(req, res) {
+app.get("/", function(req, res) {
     res.sendFile(indexPath);
-})
+});
 
 //  Serves json response object to get requests at /json endpoint
-app.get('/json', function(req, res) {
-    //  set json response object based on MESSAGE STYLE .env variable
-    if (process.env.MESSAGE_STYLE === "uppercase") {
-        const jsonResponse = {"message": "HELLO JSON"}
-    } else {
-        const jsonResponse = {"message": "Hello json"}
-    }
+app.get("/json", function(req, res) {
+
+  //  set json response object based on MESSAGE STYLE .env variable
+let message = "Hello json";
+
+if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase()
+} 
+
+const jsonResponse = { "message": message};
+
     res.json(jsonResponse);
-})
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
